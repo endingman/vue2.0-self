1e55c28d7bb96fcf790735f5979716e2c7a16dc3
panjianlin <447665722@shsnc.com>
2016-11-09 12:27:44 +0800

    添加html内使用stylus的例子

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,12 @@
 </template>
 
 
-<style>
-#app {
+<style lang="stylus" rel="stylesheet/stylus">
+#app
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
 </style>
